ff5387f927093e3060059760399aed352bf86812
tzadiki omer <tzadikiomer@gmail.com>
2016-12-07 14:26:09 +0200

    add utils to handle

--- utils/utils.js
+++ utils/utils.js
@@ -15,4 +15,40 @@ export default class Utils {
     get KeyMapping() {
         return keymapping;
     }
+
+
+    // Get ide name and userKeys json files, and returns an array of ActionName:{actionId:***, shortcut:****}, the shortcut as string
+	getNormalizedListByIDE(ideName, userKeys) {
+		var arr = [];
+		for (action in userActionsJson) {
+			var obj = {};
+			obj.actionId = getActionIdByIde(action, ideName);
+			obj.shortcut = keyArrayToString(userkeys[action].shortCut)
+			arr[action] = obj; 
+		}
+
+		return arr;
+	}	
+	// Get keyarray and ide name and return string represent the keystroke in the ide shortcuts file.
+	keyArrayToString(keyArray, ideName) {
+		var returnVal = " ";
+		for (key in keyMap) {
+			returnVal += getKeyBy
+			Ide(key, ideName);
+			returnVal += getKeyByIde("DELIMETER", ideName);
+		}
+
+		// Return last char
+		returnVal = returnVal.substring(0, returnVal.length  - 1);
+
+		return returnVal;
+	}
+
+	getActionIdByIde(actionName, ide) {
+		return actions[actionName]["IDE"][ide];
+	}
+
+	getKeyByIde(key,ideName) {
+		return keymapping[key]["IDE"][ideName];
+	}
 };
