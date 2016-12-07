e3516d7de7aae902f6a2cba9d5e5b542e4197a45
Shahar Porat <shahar3012@gmail.com>
2016-12-07 21:28:53 +0200

    create xml keymaps files for android studio - wip

--- utils/ides/androidstudio.js
+++ utils/ides/androidstudio.js
@@ -1,4 +1,5 @@
 'use strict';
+import * as path from 'path';
 import * as IDE from 'ide';
 import Utils from '../utils';
 import * as xmlbuilder from 'xmlbuilder';
@@ -6,13 +7,14 @@ import * as defaultAndroidStudio from '../common/keybindings/androidstuido.json'
 
 export default class AndroidStudio extends IDE {
 
-    constructor(platform) {
+    constructor(platform, relativePath) {
         super();
         this.name = "androidstudio";
         this.utils = new Utils(platform);
-       
+        this.relativePath = relativePath;
+
         this.createAndroidStudioXMLPrefs = (keybindings) => {
-            var xml = xmlbuilder.create('keymap', { 'version': 1 });
+            var xml = xmlbuilder.create('keymap', { 'version': 1, 'name': "SticKeys", 'parent': "$default" });
             for (keybinding of keybindings) {
                 xml = xml.ele('action', { 'id': keybinding.action })
                     .ele('keyboard-shortcut', { 'first-keystroke': keybinding.keys });
@@ -20,14 +22,27 @@ export default class AndroidStudio extends IDE {
 
             return xml.end({ pretty: true });
         }
-    }
-    saveKeyBindings(userprefs) {
-        // fs.save(createKeyBinding());
+
+        this.createKeyBindings = (userprefs) => {
+            var keybindings = this.utils.getNormalizedListByIDE(this.name, userprefs);
+            return this.createAndroidStudioXMLPrefs(keybindings);
+        };
     }
 
-    createKeyBindings(userprefs) {
-        var keybindings = this.utils.getNormalizedListByIDE(this.name, userprefs);
-        return this.createAndroidStudioXMLPrefs(keybindings);
+    saveKeyBindings(userprefs) {
+
+        fs.mkdir(path.join(this.relativePath, 'keymaps'), (err) => {
+            if (err) throw err;
+
+            fs.writeFile(path.join(this.relativePath, 'keymaps', 'SticKeys.xml'), this.createKeyBindings(userprefs), (err) => {
+                if (err) throw err;
+                console.log('It\'s saved!');
+            });
+            
+            console.log('It\'s saved!');
+        });
+
+
     }
 
     loadDefaultKeyBindings() {
