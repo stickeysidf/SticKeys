ded16d50ea68f2b3eeb554374c014fbedbd198cc
Yoav <yoav.codish2@gmail.com>
2016-12-07 18:55:50 +0200

    Almost finished vscode.js

--- utils/ides/vscode.js
+++ utils/ides/vscode.js
@@ -9,24 +9,39 @@ export default class VSCode extends IDE {
         super();
         this.name = "vscode";
         this.utils = new Utils(platform);
+
+        this.createVSCodeJSONPrefs = (keybindings) => {
+            var defualtKeyBindings = loadDefaultKeybindings();
+
+            return userprefs.map((keybinding) => {
+                keybinding['key'] = keybinding.keys;
+                keybinding['command'] = keybinding.action;
+                keybinding['when'] = defualtKeyBindings.filter((defaultKeybinding) => {
+                    if (defaultKeybinding.action === keybinding.action) {
+                        return true;
+                    }
+
+                    return false;
+                })[0].when;
+
+                delete(keybinding.keys);
+                delete(keybinding.action);
+            });
+        }
+
+        this.createKeyBindings = (userprefs) => {
+            var keybindings = this.utils.getNormalizedListByIDE(this.name, userprefs);
+            return this.createVSCodeJSONPrefs(keybindings);
+        };
     }
 
     saveKeyBindings(userprefs) {
         // fs.save(createKeyBinding());
     }
 
-    createKeyBindings(userprefs) {
-
-        var json = userprefs.map((keybinding) => {
-            keybinding['key'] = keybinding.keys;
-            keybinding['command'] = keybinding.action;
-            keybinding['when'] = defaultKeyBindings.;
-        });
-
-    }
-
-    loadDefaultKeyBindings() {
-        return defaultVSCode;
+    loadDefaultKeybindings() {
+        var defualtKeybindings = this.utils.getNormalizedListByIDE(this.name, defaultVSCode);
+        return defualtKeybindings;
     }
 
 
