800dc35c8b6f108dd75b98a3695bdd0f53d69849
Sagi Hatzabi <Sagi363@gmail.com>
2016-12-07 11:59:09 +0200

    White window

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const electron = require('electron')
+const fs = require('fs');
+
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
@@ -10,10 +12,37 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+var backgroundColor = '#fff'
 
 function createWindow () {
+  const userAgent = '(Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36';
+  const rammeDesktopIcon = path.join(__dirname, 'images/Icon.png');
+  const maxWidthValue = 3000;
+  const minWidthValue = 900;
+  const maxHeighthValue = 1000;
+  const minHeightValue = 660;
+  
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    title: app.getName(),
+    show: true,
+    minWidth: minWidthValue,
+    maxWidth: maxWidthValue,
+    minHeight: minHeightValue,
+    maxHeight: maxHeighthValue,
+    width: minWidthValue,
+    height: minHeightValue,
+    icon: process.platform === 'linux' && sticKeysDesktopIcon,
+    titleBarStyle: 'hidden-inset',
+    backgroundColor: backgroundColor,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'browser.js'),
+      nodeIntegration: false
+    }
+  });
+
+  mainWindow.webContents.setUserAgent(userAgent);
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -23,21 +52,41 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
+    // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  mainWindow.on('page-title-updated', e => {
+    e.preventDefault();
+  });
+
+  return mainWindow;
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  mainWindow = createWindow();
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools()
+
+  const page = mainWindow.webContents;
+
+  page.on('dom-ready', () => {
+    page.insertCSS(fs.readFileSync(path.join(__dirname, 'browser.css'), 'utf8'));
+
+    mainWindow.show();
+  });
+
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
